b559373d504bdeea399a2a96275d22c481395012
Jacob Odoyo <jacobodoyo56@gmail.com>
2024-06-30 16:36:43 +0300

    Committing changes in submodule EmployeeMS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build',  // Specify the output directory
+    rollupOptions: {
+      input: './index.html'  // Ensure this points to your entry HTML file
+    }
+  }
+});
+
